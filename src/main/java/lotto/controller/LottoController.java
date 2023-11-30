8fe9bd3e9a77128854435b40db1eb0c150b92fae
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-30 14:33:20 +0900

    move : Lotto 클래스 model 패키지로 이동

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,7 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.Lotto;
+import lotto.model.Lotto;
 import lotto.model.vo.BonusNumber;
 import lotto.model.vo.BuyAmount;
 import lotto.util.InputConverter;
